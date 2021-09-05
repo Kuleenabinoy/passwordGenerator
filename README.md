f04dde3c7ae45e71f247d42699ef3955f33457ab
leena <annachandy123@gmail.com>
2021-09-05 15:13:48 +1000

    updated generatepassword

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # passwordGenerator
+UPDATIONS DONE @.js file
+
+if loop is added to check whether the number entered is 0,empty space ;
+
+promt and confirm used to take values from user
+
+if loop is added to check atleast 2 selectors are true for continue with password generation
+
+variable declared with   4 strings  used for password generation-uppercase,lowercase,number,special character()
+
+different option variable  is made by merging different  possible options(option1,2,3,4,5,6)
+
+random method is used to select a random choice using different functions 
+
+generate function is used to generate random selection
+
+write function is used to write it to the text area
+
+github repository:https://github.com/Kuleenabinoy/passwordGenerator
+
+deployed website:
--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 
-//Checking whether password length is 0 or null
+//Taking user input using prompt and confirm
+
 var passwordNo=prompt ("enter no of characters in password");
-if((passwordNo==0)||(passwordNo===null))
-{alert("Kindly Enter no of digits for password");
-exit();
+var noOfchar=0;
+noOfchar=passwordNo;
+if((noOfchar==='0')||(noOfchar===' ')||(noOfchar===null))// checking whether user choice is 0 or empty string or null string
+ {
+   alert("Kindly Enter Number of digits for password");
 
-}
-else {
 
+ }
+ else{
 var upperCaseselector=confirm("Do you want to have uppercase in password");
 var lowerCaseselector=confirm("Do you want to have lowercase in password");
 var wholeNumberselector=confirm("Do you want to have Number in password");
 var specialCharacterselector=confirm("Do you want to have special  charcater in password");
-}
+ }
 
- 
+
  //checking for atleast 2 selectors are true to continue
- if(((lowerCaseselector!=true)||(upperCaseselector!=true)||(wholeNumberselector!=true)) &&((wholeNumberselector!=true)||(specialCharacterselector!=true)))
+ if((((lowerCaseselector!==true)||(upperCaseselector!==true))||(wholeNumberselector!==true)) &&((wholeNumberselector!==true)||(specialCharacterselector!==true)))
   {
     alert(" You need to select atleast two options");
 
@@ -25,155 +28,101 @@ else{
   console.log(passwordNo);
 }
 
-  
-var specialCharacters = '!@#$%^&*(){}[]=<>/,.' ;
-  
-
-  var upperCasedCharacters =[
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z',
-  ];
-  var lowerCasedCharacters =[
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-  ];
-  var numericCharacters =['0', '1' ,'2', '3', '4', '5', '6', '7', '8', '9'];
+  //declaring variables with alphabets ,number and special characters
+   var specialCharacters = '!@#$%^&*(){}[]=<>/,.' ;
+  var upperCasedCharacters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var lowerCasedCharacters ='abcdefghijklmnopqrstuvwxyz';
+  var numericCharacters ='0123456789';
  var sC=specialCharacters;
  var nM=numericCharacters;
  var lC=lowerCasedCharacters;
  var uC=upperCasedCharacters;
-var password=0;
-option1=sC+nM+lC+uC;
+var password1='';
+option1=sC+nM+lC+uC;//all select
 option2=sC+nM; //special numeric
 option3=lC+uC; //lower upper
 option4=nM+lC; //numeric,lowercase
 option5=sC+nM+lC; //special numeric lower
 option6=nM+lC+uC; //numeric lower uppercase
-//console.log(option1);
+
+// Add event listener to generate button    
 var generateBtn = document.querySelector("#generate");
 //generateBtn.addEventListener("click",generatePassword());
 generateBtn.addEventListener("click", writePassword());
-// Add event listener to generate button
 
 
+//function generatePassword() to generate password after checking options using if loop
+//after checking option it call corresponding random password generate function
 function generatePassword()
 {
-
-if((wholeNumberselector==true)&&(specialCharacterselector==true))
-{for(var i=0;i<passwordNo-1;i++)
+  if(((wholeNumberselector===true)&&(specialCharacterselector===true))&&((lowerCaseselector===true)&&(upperCaseselector===true)))
+  {
+    for (var i=0;i<noOfchar;i++)
+   { password1 += getRandomall();
+    console.log( "password1" ,password1);//checking whethr loop working
+   }
+  }
+  else if (((lowerCaseselector===true)&&(wholeNumberselector===true))&&(specialCharacterselector===true))
+  {
+      for (var i=0;i<noOfchar;i++)
+      {  password1 +=getRandomSpecialnumericlower();
+        console.log( "password2" ,password1);
+      
+      }
+  
+  }
+  else if(((lowerCaseselector===true)&&(upperCaseselector===true))&&(wholeNumberselector===true))
+  {
+    for (var i=0;i<noOfchar;i++)
+      { password1 +=getRandomNumericlowerupper();
+        console.log( "password3" ,password1);
+      }
+      
+  }
+  
+else if((wholeNumberselector===true)&&(specialCharacterselector===true))
+{
+  for(var i=0;i<noOfchar;i++)
   { 
-    password += getrandomSpecialnumber();
-    
+     password1 += getrandomSpecialnumber();
+    console.log( "password4" ,password1);
     
   }
 }
-else if((wholeNumberselector==true)&&(specialCharacterselector==true)&&(lowerCaseselector==true)&&(upperCaseselector==true))
-{for (var i=0;i<passwordNo-1;i++)
-{password += getRandomall();
-  
-   
-}
-}
-else if((lowerCaseselector==true)&&(upperCaseselector==true))
-{
-  for (var i=0;i<passwordNo-1;i++)
-    {password+=getRandomLowerupper();
-      
-    }
-    
-}
-else if((wholeNumberselector==true)&&(lowerCaseselector==true))
-{
-  for (var i=0;i<passwordNo-1;i++)
-    {password+=getRandomNumericlower();
-      
-    }
-    
-}
-else if((lowerCaseselector==true)&&(upperCaseselector==true))
-{
-  for (var i=0;i<passwordNo-1;i++)
-    {password+=getRandomLowerupper();
-      
-    }
-    
-}
-else if((lowerCaseselector==true)&&(upperCaseselector==true)&&(wholeNumberselector==true))
-{
-  for (var i=0;i<passwordNo-1;i++)
-    {password+=getRandomNumericlowerupper();
-     
-    }
-    
-}
-else ((lowerCaseselector==true)&&(wholeNumberselector==true)&&(specialCharacterselector==true))
-{
-  for (var i=0;i<passwordNo-1;i++)
-    {password+=getRandomSpecialnumericlower();
-      
-    
-    }
 
+else if((lowerCaseselector===true)&&(upperCaseselector===true))
+{
+  for (var i=0;i<noOfchar;i++)
+    { password1 +=getRandomLowerupper();
+      console.log( "password5" ,password1);
+    }
+    
 }
+else if((wholeNumberselector===true)&&(lowerCaseselector===true))
+{
+  for (var i=0;i<noOfchar;i++)
+    { password1 +=getRandomNumericlower();
+      console.log( "password6" ,password1);
+    }
+    
 }
+
+
+}// end of generate function
+
+// function  writepassword() to write in text area
 
 function writePassword()
-{generatePassword();
-console.log( "password" ,password);
-var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+{ 
+  generatePassword();
+  console.log( "passwordfinal" ,password1);
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password1;
 
 }
+//end of write function
+
+//Random string generating functions 6 functions are added for 6 possible options
 
 function getRandomall()
 {
@@ -186,12 +135,12 @@ function getRandomall()
   
 }
 function getRandomLowerupper()
-{//const randomElement = array[Math.floor(Math.random() * array.length)];
+{
   return option3[Math.floor(Math.random()*option3.length)];
 }
 function getRandomNumericlower()
 {
-  return option4[Math.floor(Math.random()*option4.length)];
+  return  option4[Math.floor(Math.random()*option4.length)];
 }
 
 function getRandomSpecialnumericlower()
@@ -203,6 +152,7 @@ function getRandomNumericlowerupper()
   return option6[Math.floor(Math.random()*option6.length)];
 }
 
+//end of random string generating functions
 
 
 
@@ -211,39 +161,6 @@ function getRandomNumericlowerupper()
 
 
 
-
-
-
-/*function getrandomLower()
-{
-  return String.fromCharCode(Math.floor(Math.random()*26)+ 97);
-}
-console.log(getrandomLower());
-
-function getrandomUpper()
-{
-  return String.fromCharCode(Math.floor(Math.random()*26)+ 65);
-}
-console.log(getrandomUpper());
-function getrandomNumber()
-{
-  return String.fromCharCode(Math.floor(Math.random()*10)+ 48);
-  
-}
-console.log(getrandomNumber());
-
-function getrandomSymbol()
-{var symbols='!@#$%^&*(){}[]=<>/,.' ;
-  return symbols[Math.floor(Math.random()*symbols.length)];
-  
-}
-
-console.log(getrandomSymbol());*/
-
-// Assignment Code
-
-
-// Write password to the #password input
 
 
 
